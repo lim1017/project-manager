3c089f1bf5ee0dabcb766915cc2cc6329be6fec0
tom <lim1017@hotmail.com>
2023-07-12 15:48:59 -0400

    work around for passing date obj into client components, refactoring create task

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -2,16 +2,14 @@ import GreetingsSkeleton from "@/components/GreetingSkeleton";
 import Greetings from "@/components/Greetings";
 import NewProject from "@/components/NewProject";
 import Projects from "@/components/Projects";
-import TaskCard from "@/components/TaskCard";
+import TaskCardContainer from "@/components/TaskCardContainer";
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
 import Providers from "@/lib/providers/Provider";
-import { store } from "@/store";
-import { setProjects } from "@/store/projectSlice";
 import { cookies } from "next/headers";
 import { Suspense } from "react";
 
-const getProjects = async () => {
+export const getProjects = async () => {
   const user = await getUserFromCookie(cookies());
 
   const projects = await db.project.findMany({
@@ -32,7 +30,7 @@ export default async function Page() {
   const projects = await getProjects();
 
   //saving projects to store in server side component
-  store.dispatch(setProjects(projects));
+  // store.dispatch(setProjects(projects));
 
   return (
     <div className="h-full overflow-y-auto pr-6 w-full">
@@ -44,16 +42,22 @@ export default async function Page() {
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           <Providers>
-            <Projects projects={projects} />
+            <Projects
+              //work around for can not pass date objs to client
+              projects={JSON.parse(JSON.stringify(projects))}
+              data-superjson
+            />
           </Providers>
           <div className="w-1/3 p-3">
             <NewProject />
           </div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
-          <div className="w-full">
-            <TaskCard />
-          </div>
+          {projects.length ? (
+            <div className="w-full">
+              <TaskCardContainer />
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
