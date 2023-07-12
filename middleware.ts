3ec3ce408f360f15573f1bcb870577fd0e0d1c4e
tom <lim1017@hotmail.com>
2023-07-12 11:28:54 -0400

    added redirect from login/register if user is logged in middleware

--- middleware.ts
+++ middleware.ts
@@ -15,6 +15,17 @@ const verifyJWT = async (jwt: string | Uint8Array) => {
 export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
+  //@ts-expect-error ts error
+  const jwt = req.cookies.get(process.env.COOKIE_NAME);
+
+  if (
+    (jwt && pathname.startsWith("/signin")) ||
+    pathname.startsWith("/register")
+  ) {
+    req.nextUrl.pathname = "/home";
+    return NextResponse.redirect(req.nextUrl);
+  }
+
   //allows these paths to be accessed without a jwt
   if (
     pathname.startsWith("/_next") ||
@@ -26,9 +37,6 @@ export default async function middleware(req: NextRequest) {
   ) {
     return NextResponse.next();
   }
-
-  //@ts-expect-error ts error
-  const jwt = req.cookies.get(process.env.COOKIE_NAME);
 
   // if (pathname.startsWith("/signin") || pathname.startsWith("/register")) {
   //   if (jwt) {
